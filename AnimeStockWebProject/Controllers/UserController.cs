39f29f54659511a55e8e93f4d0f7111f7e2b366f
darkwinner3 <martinevtimov03@abv.bg>
2024-03-06 21:57:50 +0200

    -- added ability to save profile details -- able to save profile picture

---
+++ AnimeStockWebProject/Controllers/UserController.cs
@@ -0,0 +1,112 @@
+﻿namespace AnimeStockWebProject.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using AnimeStockWebProject.Core.Contracts;
+    using AnimeStockWebProject.Infrastructure.Data.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Caching.Memory;
+    using static Common.NotificationKeys;
+    using static Common.NotifiactionMessages;
+    using static Common.GeneralAplicaitonConstants;
+    using AnimeStockWebProject.Extensions;
+    using AnimeStockWebProject.Core.Models.User;
+    using System.Security.Claims;
+
+    public class UserController : Controller
+    {
+        private readonly IUserService userService;
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+        private readonly IMemoryCache memoryCache;
+        public UserController(IUserService userService, UserManager<User> userManager,
+            SignInManager<User> signInManager, IMemoryCache memoryCache)
+        {
+            this.userService = userService;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UserProfile(Guid id)
+        {
+            if (id != this.User.GetId())
+            {
+                return Unauthorized();
+            }
+            try
+            {
+                string cacheKey = string.Format(UserInfoCacheKey, id);
+                UserInfoViewModel userInfoViewModel = this.memoryCache.Get<UserInfoViewModel>(cacheKey);
+                if (userInfoViewModel == null)
+                {
+                    userInfoViewModel = await userService.GetUserInfoByIdAsync(id);
+                    MemoryCacheEntryOptions options = new MemoryCacheEntryOptions()
+                        .SetAbsoluteExpiration(TimeSpan.FromMinutes(UserInfoCacheDuration));
+                    this.memoryCache.Set(cacheKey, userInfoViewModel, options);
+                }
+                return View(userInfoViewModel);
+            }
+            catch (Exception)
+            {
+                TempData[ErrorMessage] = DefaultErrorMessage;
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserProfile(UserInfoViewModel userInfoViewModel)
+        {
+            if(userInfoViewModel.ProfilePictureFile != null)
+            {
+                string fileExtension = Path.GetExtension(userInfoViewModel.ProfilePictureFile.FileName).ToLower();
+
+                if (!string.IsNullOrWhiteSpace(userInfoViewModel.ProfilePicturePath))
+                {
+                    await userService.DeleteUserProfilePictureAsync(this.User.GetId(), userInfoViewModel.ProfilePicturePath);
+                }
+
+                userInfoViewModel.ProfilePicturePath = await userService.UploadUserImageAsync(userInfoViewModel, this.User.GetId());
+
+                try
+                {
+                    User user = await userManager.FindByIdAsync(this.User.GetId().ToString());
+                    await userService.SaveUserInfoAsync(this.User.GetId(), userInfoViewModel);
+                    Claim userNameClaim = new Claim("ProfilePicturePath", userInfoViewModel.ProfilePicturePath);
+
+                    if(this.User.HasClaim(c => c.Type == "ProfilePicturePath"))
+                    {
+                        Claim claim = this.User.Claims.FirstOrDefault(c => c.Type == "ProfilePicturePath");
+                        await userManager.RemoveClaimAsync(user, claim);
+                    }
+
+                    //TempData[SuccessMessage] = SuccessfullyUpdatedAccount;
+                    await userManager.AddClaimAsync(user, userNameClaim);
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    this.memoryCache.Remove(string.Format(UserInfoCacheKey, userInfoViewModel.Id));
+                    return RedirectToAction("UserProfile", "User");
+                }
+
+                catch (Exception)
+                {
+                    TempData[ErrorMessage] = DefaultErrorMessage;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            try
+            {
+                await userService.SaveUserInfoAsync(this.User.GetId(), userInfoViewModel);
+                TempData[SuccessMessage] = SuccessfullyUpdatedAccount;
+                string cacheKey = string.Format(UserInfoCacheKey, userInfoViewModel.Id);
+                this.memoryCache.Remove(cacheKey);
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception)
+            {
+                TempData[ErrorMessage] = DefaultErrorMessage;
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
