59a1ea1518ccba399c93c293f84fb531cd1d1b71
darkwinner3 <martinevtimov03@abv.bg>
2024-03-09 00:46:02 +0200

    -- created book controller -- added pager and unfinished sorting

---
+++ AnimeStockWebProject/Controllers/BookController.cs
@@ -0,0 +1,55 @@
+﻿using AnimeStockWebProject.Core.Contracts;
+using AnimeStockWebProject.Core.Models.Book;
+using AnimeStockWebProject.Core.Models.BookTags;
+using AnimeStockWebProject.Core.Models.Pager;
+using AnimeStockWebProject.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace AnimeStockWebProject.Controllers
+{
+    using static Common.GeneralAplicaitonConstants;
+    public class BookController : Controller
+    {
+        private readonly ITagService tagService;
+        private readonly IBookService bookService;
+        private readonly IMemoryCache memoryCache;
+        public BookController(ITagService tagService, IBookService bookService,
+            IMemoryCache memoryCache)
+        {
+            this.tagService = tagService;
+            this.bookService = bookService;
+            this.memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Books([FromQuery] BookQueryViewModel bookQueryViewModel)
+        {
+            if (bookQueryViewModel.currentPage < 1)
+            {
+                bookQueryViewModel.currentPage = 1;
+            }
+            
+            Guid? userId = null;
+            if (User.Identity.IsAuthenticated)
+            {
+                userId = User.GetId();
+            }
+            Pager pager = new Pager(await bookService.GetCountAsync(bookQueryViewModel), bookQueryViewModel.currentPage);
+            bookQueryViewModel.Pager = pager;
+            AllBooksSortedDataModel sortedBooks = await bookService.GetAllBooksSortedDataModelAsync(userId, bookQueryViewModel);
+            bookQueryViewModel.BookViewModels = sortedBooks.Books;
+            IEnumerable<TagViewModel> tags = this.memoryCache.Get<IEnumerable<TagViewModel>>(BookTagsCacheKey);
+            if (tags == null)
+            {
+                tags = await tagService.GetAllTagsAsync();
+                MemoryCacheEntryOptions cacheOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(BookTagsCacheDuration));
+                this.memoryCache.Set(BookTagsCacheKey, tags, cacheOptions);
+            }
+            bookQueryViewModel.BookTags = tags;
+
+            return View(bookQueryViewModel);
+        }
+    }
+}
