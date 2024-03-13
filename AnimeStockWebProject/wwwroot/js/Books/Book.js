46663e4ab9118f121c684aff578e2b9745ed66b9
darkwinner3 <martinevtimov03@abv.bg>
2024-03-13 23:30:21 +0200

    -- added a JS file for dynamic filtering, when pressing on the specific book types and print types on the book details

---
+++ AnimeStockWebProject/wwwroot/js/Books/Book.js
@@ -0,0 +1,24 @@
+﻿function selectPrintType(printType) {
+    var printTypeRadios = document.querySelectorAll('input.sort-option');
+    printTypeRadios.forEach(function (radio) {
+        var label = radio.parentElement.innerText.trim();
+        if (label === printType) {
+            radio.checked = true;
+            radio.dispatchEvent(new Event('change')); // Trigger change event
+            return;
+        }
+    });
+}
+
+function selectBookType(bookType) {
+    var bookTypeOption = document.querySelectorAll('input.checkBox');
+    bookTypeOption.forEach(function (checkBox) {
+        var label = checkBox.parentElement.innerText.trim();
+        if (label === bookType) {
+            checkBox.checked = true;
+            checkBox.dispatchEvent(new Event('change')); // Trigger change event
+            return;
+        }
+    });
+}
+
