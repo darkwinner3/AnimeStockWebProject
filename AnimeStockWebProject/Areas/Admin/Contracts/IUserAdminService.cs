79432efc3d1144788b54992e8473a9edf8318339
darkwinner3 <martinevtimov03@abv.bg>
2024-04-02 21:30:19 +0300

    -- added service for getting all users

---
+++ AnimeStockWebProject/Areas/Admin/Contracts/IUserAdminService.cs
@@ -0,0 +1,9 @@
+﻿using AnimeStockWebProject.Areas.Admin.Models.User;
+
+namespace AnimeStockWebProject.Areas.Admin.Contracts
+{
+    public interface IUserAdminService
+    {
+        Task<IEnumerable<UsersViewModel>> GetAllUsersAsync();
+    }
+}
---
+++ AnimeStockWebProject/Areas/Admin/Services/UserAdminService.cs
@@ -0,0 +1,30 @@
+﻿using AnimeStockWebProject.Areas.Admin.Contracts;
+using AnimeStockWebProject.Areas.Admin.Models.User;
+using AnimeStockWebProject.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimeStockWebProject.Areas.Admin.Services
+{
+    public class UserAdminService : IUserAdminService
+    {
+        private readonly AnimeStockDbContext animeStockDbContext;
+
+        public UserAdminService(AnimeStockDbContext animeStockDbContext)
+        {
+            this.animeStockDbContext = animeStockDbContext;
+        }
+        public async Task<IEnumerable<UsersViewModel>> GetAllUsersAsync()
+        {
+            var users = await this.animeStockDbContext.Users.Select(u => new UsersViewModel()
+            {
+                Email = u.Email,
+                FirstName = u.FirstName,
+                Id = u.Id,
+                Joined = u.JoinTime,
+                UserName = u.UserName
+            })
+                .ToArrayAsync();
+            return users;
+        }
+    }
+}
