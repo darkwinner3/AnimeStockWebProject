24a7b77c7df28f305bc227fd7ee7ab6fcf40f512
darkwinner3 <martinevtimov03@abv.bg>
2024-04-02 21:27:21 +0300

    -- added a decimal model binder that accepts input from user with different locales

---
+++ AnimeStockWebProject/ModelBinders/DecimalModelBinder/DecimalModelBinder.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Globalization;
+
+namespace AnimeStockWebProject.ModelBinders.DecimalModelBinder
+{
+    public class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            ValueProviderResult result = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if (result != ValueProviderResult.None && !string.IsNullOrWhiteSpace(result.FirstValue))
+            {
+                decimal parsed = 0m;
+                bool succeeded = false;
+
+                try
+                {
+                    string decValue = result.FirstValue;
+                    decValue = decValue.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                    decValue = decValue.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    parsed = Convert.ToDecimal(decValue);
+                    succeeded = true;
+                }
+                catch (FormatException fe)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+                }
+
+                if (succeeded)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(parsed);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
