62c1755ab3472ef3c9af2ee94f55e36cb507eb4f
darkwinner3 <martinevtimov03@abv.bg>
2024-04-06 23:56:35 +0300

    -- replaced the js function for displaying selected file names in a js file -- added function that removes the quntity if book is digital -- added delete picture ajax function(work in progress)

---
+++ AnimeStockWebProject/wwwroot/js/Admin/Books/Book.js
@@ -0,0 +1,68 @@
+﻿document.getElementById('CoverImg').addEventListener('change', function () {
+    var files = this.files;
+    var indicator = document.getElementById('coverImgIndicator');
+    if (files.length > 0) {
+        indicator.textContent = files[0].name;
+    } else {
+        indicator.textContent = '';
+    }
+});
+
+document.getElementById('Pictures').addEventListener('change', function () {
+    var files = this.files;
+    var indicator = document.getElementById('picturesIndicator');
+    if (files.length > 0) {
+        indicator.textContent = files.length + ' file(s) selected';
+    } else {
+        indicator.textContent = '';
+    }
+});
+
+document.getElementById('BookFile').addEventListener('change', function () {
+    var files = this.files;
+    var indicator = document.getElementById('bookFileIndicator');
+    if (files.length > 0) {
+        indicator.textContent = files[0].name;
+    } else {
+        indicator.textContent = '';
+    }
+});
+
+
+function toggleBookQuantityVisibility() {
+    var digitalPrintType = document.getElementById("DigitalPrintType");
+    var quantityFormGroup = document.getElementById("quantity-form");
+
+    if (digitalPrintType.checked) {
+        quantityFormGroup.style.display = "none";
+        document.getElementById("Quantity").value = 0;
+    } else {
+        quantityFormGroup.style.display = "block";
+    }
+}
+
+document.getElementById("DigitalPrintType").addEventListener("change", toggleBookQuantityVisibility);
+document.getElementById("PhysicalPrintType").addEventListener("change", toggleBookQuantityVisibility);
+
+toggleBookQuantityVisibility();
+
+//ajax request for for deleting pictures inside the form
+function deletePicture(imgId) {
+    const url = `Admin/Picture/Delete/${imgId}`;
+
+    fetch(url, {
+        method: 'POST',
+        headers: { 'RequestVerificationToken': csrfToken },
+    })
+        .then(Response => Response.json())
+        .then(data => {
+            if (data.success) {
+                const deletedImg = document.getElementById(`image_${imgId}`);
+                if (deletedImg) {
+                    deletedImg.parentNode.removeChild(deletedImg);
+                }
+            }
+        })
+        .catch((error) => console.error(error));
+
+}
