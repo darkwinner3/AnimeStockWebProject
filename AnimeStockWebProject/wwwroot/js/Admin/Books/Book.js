120d70fd6b413fb77f6d13d4ef22f8d41053e9ff
darkwinner3 <martinevtimov03@abv.bg>
2024-04-07 23:52:11 +0300

    -- finished ajax function for deleting and recovering images withou the need for refreshing of the page

--- AnimeStockWebProject/wwwroot/js/Admin/Books/Book.js
+++ AnimeStockWebProject/wwwroot/js/Admin/Books/Book.js
@@ -8,6 +8,26 @@
     }
 });
 
+document.getElementById('NewCoverImg').addEventListener('change', function () {
+    var files = this.files;
+    var indicator = document.getElementById('newCoverImgIndicator');
+    if (files.length > 0) {
+        indicator.textContent = files[0].name;
+    } else {
+        indicator.textContent = '';
+    }
+});
+
+document.getElementById('NewPictures').addEventListener('change', function () {
+    var files = this.files;
+    var indicator = document.getElementById('newPicturesIndicator');
+    if (files.length > 0) {
+        indicator.textContent = files.length + ' file(s) selected';
+    } else {
+        indicator.textContent = '';
+    }
+});
+
 document.getElementById('Pictures').addEventListener('change', function () {
     var files = this.files;
     var indicator = document.getElementById('picturesIndicator');
@@ -29,6 +49,7 @@ document.getElementById('BookFile').addEventListener('change', function () {
 });
 
 
+
 function toggleBookQuantityVisibility() {
     var digitalPrintType = document.getElementById("DigitalPrintType");
     var quantityFormGroup = document.getElementById("quantity-form");
@@ -46,23 +67,67 @@ document.getElementById("PhysicalPrintType").addEventListener("change", toggleBo
 
 toggleBookQuantityVisibility();
 
-//ajax request for for deleting pictures inside the form
-function deletePicture(imgId) {
-    const url = `Admin/Picture/Delete/${imgId}`;
+//ajax requests for deleting and recovering pictures inside the form
+async function deletePicture(pictureId, event) {
+    event.preventDefault();
 
-    fetch(url, {
-        method: 'POST',
-        headers: { 'RequestVerificationToken': csrfToken },
-    })
-        .then(Response => Response.json())
-        .then(data => {
-            if (data.success) {
-                const deletedImg = document.getElementById(`image_${imgId}`);
-                if (deletedImg) {
-                    deletedImg.parentNode.removeChild(deletedImg);
-                }
+    const url = `/Admin/Picture/Delete?id=${pictureId}`;
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'RequestVerificationToken': csrfToken
+            },
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            const buttonClicked = event.target;
+
+            if (buttonClicked.textContent == "Recover") {
+                buttonClicked.textContent = 'Delete';
+                buttonClicked.setAttribute('onclick', `deletePicture(${pictureId}, event)`);
+
             }
-        })
-        .catch((error) => console.error(error));
+            else {
+                buttonClicked.textContent = 'Recover';
+                buttonClicked.setAttribute('onclick', `recoverPicture(${pictureId}, event)`);
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
 
+async function recoverPicture(pictureId, event) {
+    event.preventDefault();
+
+    const url = `/Admin/Picture/Recover?id=${pictureId}`;
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'RequestVerificationToken': csrfToken
+            },
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            const buttonClicked = event.target;
+
+            if (buttonClicked.textContent == "Recover") {
+                buttonClicked.textContent = 'Delete';
+                buttonClicked.setAttribute('onclick', `deletePicture(${pictureId}, event)`);
+
+            }
+            else {
+                buttonClicked.textContent = 'Recover';
+                buttonClicked.setAttribute('onclick', `recoverPicture(${pictureId}, event)`);
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
 }
